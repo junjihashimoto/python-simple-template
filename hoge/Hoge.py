c240991acd3811154a5bd16c954412d2cdaabf25
Junji Hashimoto <junji.hashimoto@gmail.com>
2018-02-13 19:48:14 +0900

    Add tensorflow

--- hoge/Hoge.py
+++ hoge/Hoge.py
@@ -4,6 +4,17 @@
 
 """
 
+import tensorflow as tf
+
+def myreshape(x):
+    """
+    >>> x = tf.zeros([10,56,56,1],name='x')
+    >>> myreshape(x)
+    <tf.Tensor 're:0' shape=(40, 28, 28, 1) dtype=float32>
+    
+    """
+    return tf.reshape(x, [-1, 28, 28, 1], name="re")
+
 
 class Piyo:
     def __init__(self):
--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ from setuptools import setup, find_packages
 setup(
     name = "hoge",
     version = "0.1",
-    packages = find_packages()
+    packages = find_packages(),
+    install_requires = [
+        'numpy',
+        'tensorflow'
+    ]
 )
