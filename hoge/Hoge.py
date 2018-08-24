cdfb059d6d9b1740044208cd58948e3b6826ebe8
Junji Hashimoto <junji.hashimoto@gree.net>
2018-08-24 19:48:17 +0900

    Add quickcheck like function and detecting not tested functions

--- hoge/Hoge.py
+++ hoge/Hoge.py
@@ -5,13 +5,65 @@
 """
 
 import tensorflow as tf
+import random
+
+
+def prop_int_eq(fn):
+    """
+    >>> prop_int_eq(lambda x: (1 <= x and x <= 100))
+    True
+    """
+    num_dim = 10
+    max_dim = 100
+    min_dim = 1
+    random_dim = list(map(lambda x: random.randint(min_dim, max_dim), range(1, num_dim) ))
+    result_list = list(map(lambda x: fn(x),random_dim))
+    return all(result_list)
+
+def shape_eq(x,out_shape):
+    return x.shape == tf.TensorShape(out_shape)
+
+def type_eq(x,out_dtype):
+    return x.dtype == out_dtype
+
+def ttype_eq(x,out_shape,out_dtype):
+    return shape_eq(x,out_shape) and type_eq(x,out_dtype)
+
+def inout_eq(fn,in_shape,in_dtype,out_shape,out_dtype):
+    v = fn(tf.zeros(in_shape,dtype=in_dtype))
+    r = ttype_eq(v,out_shape,out_dtype)
+    if not r:
+        print("in_shape:")
+        print(in_shape)
+        print("expected out_shape:")
+        print(out_shape)
+        print("got      out_shape:")
+        print(v.shape)
+    return r
+
 
 def myreshape(x):
     """
     >>> x = tf.zeros([10,56,56,1],name='x')
     >>> myreshape(x)
     <tf.Tensor 're:0' shape=(40, 28, 28, 1) dtype=float32>
-    
+    >>> v = myreshape(x)
+    >>> shape_eq(v,[40,28,28,1])
+    True
+    >>> type_eq(v,tf.float32)
+    True
+    >>> ttype_eq(v,[40,28,28,1],tf.float32)
+    True
+    >>> ttype_eq(v,[40,28,28,1],tf.float32)
+    True
+    >>> inout_eq(myreshape,\
+                 in_shape=[10,56,56,1],in_dtype=tf.float32,\
+                 out_shape=[40,28,28,1],out_dtype=tf.float32)
+    True
+    >>> prop_int_eq(lambda n: inout_eq(myreshape,\
+                                       in_shape=[n,56,56,1],in_dtype=tf.float32,\
+                                       out_shape=[n*4,28,28,1],out_dtype=tf.float32))
+    True
     """
     return tf.reshape(x, [-1, 28, 28, 1], name="re")
 
--- tests/test_hoge.py
+++ tests/test_hoge.py
@@ -1,7 +1,35 @@
 from hoge import Hoge
 import pytest
 
-@pytest.mark.django_db(transaction=True)
+import inspect
+import re
+import sys
+
+
+def getNotTestFunctions(package):
+    members = inspect.getmembers(package)
+    members = filter((lambda m: inspect.isfunction(m[1]) ),members)
+    v = []
+    for (name,ptr) in members:
+        s = inspect.getfullargspec(ptr)
+        if ptr.__doc__ == None or \
+           ( ptr.__doc__.count('shape_eq') == 0 or \
+             ptr.__doc__.count('inout_eq') == 0 or \
+             ptr.__doc__.count('inout_eq') == 0 ) :
+          v.append(name)
+    return v
+
 def test_hoge():
   obj = Hoge.Piyo()
   assert 1 == obj.one()
+
+
+def test_detect_not_tested_functions():
+  not_tested_functions = [
+    "inout_eq",
+    "prop_int_eq",
+    "shape_eq",
+    "ttype_eq",
+    "type_eq"
+  ]
+  assert getNotTestFunctions(Hoge) == not_tested_functions
